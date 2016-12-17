9f2a178f9c3ef35e727a574d69939e71a21ea709
Pab Iy <telimpioelpatio@yahoo.com.ar>
2016-12-17 00:01:41 -0300

    promp types

--- brancha.js
+++ brancha.js
@@ -4,80 +4,24 @@ var inquirer = require('inquirer');
 var shell = require('shelljs');
 var config = require('config');
 var manageBranch = require('./lib/manageBranch');
-var promptTypes = require('./lib/promptTypes');
+var types  = require('./lib/promptTypes');
 
-let indexFlag = '--create';
+var indexFlag = '--create';
 
 process.argv.forEach(function (val, index, array) {
-  if (val == '--create' || val == '--rename' ) {
+  if (!val.indexOf('--') < 0  ) {
     indexFlag = val.replace('--', '');
   }
 });
 
 inquirer.prompt([
-  {
-    type: 'list',
-    name: 'prefix',
-    message: 'Branch Type?',
-    choices: config.get('config.prefix'),
-    validate: function (answer) {
-      if (answer.length < 1) {
-        return 'Debe elegir al menos una opcion.';
-      }
-      return true;
-    }
-  },
-  {
-    type: 'list',
-    name: 'project',
-    message: 'Project?',
-    choices: config.get('config.project'),
-    validate: function (answer) {
-      if (answer.length < 1) {
-        return 'Debe elegir al menos una opcion.';
-      }
-      return true;
-    }
-  },
-  {
-    type: 'input',
-    name: 'issue',
-    message: 'Issue Number?',
-    validate: function (answer) {
-      var regex =  /^[0-9]*$/;
-      if (answer.length < 1) {
-        return 'Debe completar el campo con un numero.';
-      }
-      if (answer.match(regex)) {
-        return true;
-      }
-
-      return 'Debe ser un numero.';
-
-    }
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Description?',
-    validate: function (answer) {
-      if (answer.length < 1) {
-        return 'Debe completar el campo con un numero.';
-      }
-
-      return true;
-    },
-    filter: function (answer) {
-       answer = answer.replace(/["']/gi, '');
-       answer = answer.replace(/[\s]/gi, '-');
-       return answer;
-    }
-  }
+  types.list('prefix', 'Branch Type', true),
+  types.list('project', 'Project', true),
+  types.inputNumber('issue', 'Issue Number', true),
+  types.inputText('description', 'Description', true, (answer)=>answer.replace(/["']/gi, '').replace(/[\s]/gi, '-')),
 ]).then(function (answers) {
-
-  let bootstrapBranch = manageBranch(indexFlag);
-
-  let branchName = `${answers.prefix}/${answers.project}-${answers.issue}-${answers.description}`;
+  var bootstrapBranch = manageBranch(indexFlag);
+  var branchName = `${answers.prefix}/${answers.project}-${answers.issue}-${answers.description}`;
 
   shell.exec(`${bootstrapBranch}${branchName}`, {silent:true}).stdout
 });
