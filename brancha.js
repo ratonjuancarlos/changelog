59b0f4810a41d21ecf51542d2cf4533de5d1bc16
Pab Iy <telimpioelpatio@yahoo.com.ar>
2016-12-19 19:14:14 -0300

    from home

--- brancha.js
+++ brancha.js
@@ -14,6 +14,29 @@ process.argv.forEach(function (val, index, array) {
   }
 });
 
+
+// [
+//     {"name": "prefix", "type":"list", "mandatory": true, "text":"Prefix"},
+//     {"name": "prefixSeparator", "type":"separator"},
+//     {"name": "project", "type":"list", "mandatory": true, "text":"Project"},
+//     {"name": "commonSeparator", "type":"separator"},
+//     {"name": "issue", "type":"inputNumber", "mandatory": true, "text":"Issue Number"},
+//     {"name": "commonSeparator", "type":"separator"},
+//     {"name": "description", "type":"inputText", "mandatory": true, "text":"Description"}
+// ]
+
+
+var templateBranchCreate = config.get(`config.templateBranchCreate`);
+
+var inquirerObject = templateBranchCreate.map( section => {
+  if (section.type !== 'separator' && types[section.type] !== undefined) {
+    return types[section.type](section.name, section.text, section.mandatory)
+  }
+
+})
+
+console.log(inquirerObject)
+
 inquirer.prompt([
   types.list('prefix', 'Branch Type', true),
   types.list('project', 'Project', true),
@@ -21,7 +44,18 @@ inquirer.prompt([
   types.inputText('description', 'Description', true, (answer)=>answer.replace(/["']/gi, '').replace(/[\s]/gi, '-')),
 ]).then(function (answers) {
   var bootstrapBranch = manageBranch(indexFlag);
-  var branchName = `${answers.prefix}/${answers.project}-${answers.issue}-${answers.description}`;
+  // var branchName = `${answers.prefix}/${answers.project}-${answers.issue}-${answers.description}`;
+  var branchName = templateBranchCreate.map( section => {
+    if (section.type === 'separator') {
+      return config.get(`config[${section.name}]`)
+    }
+
+    return answers[section.name];
+  }).join('');
+
+  console.log(branchName)
+
+
 
   shell.exec(`${bootstrapBranch}${branchName}`, {silent:true}).stdout
 });
